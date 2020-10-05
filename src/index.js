bc32b8c58018c7017e521fd40b3bd7bc7a61810d
Ross Tanchan <ross.tanchan@gmail.com>
2020-10-05 21:28:24 +0100

    Added some of the starting logic.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 var elementFactory = function (elementStr) {
     var commands = elementStr.split('/');
-    console.log(commands[0]);
+    var topLevelElement = document.createElement(commands[0]);
+    commands.shift();
+    commands.forEach(function (commandStr) {
+        var attribute = commandStr.split('=')[0];
+        var values = commandStr.split('=')[1];
+        if (attribute.toLowerCase() === 'child') {
+            // Create child elements
+        }
+        topLevelElement.setAttribute(attribute, values);
+    });
+    return topLevelElement;
 };
-elementFactory('div/class=test');
+console.log(elementFactory('div/class=test wide/id=someid/data-name=something'));
+// Include a content which is the textcontent
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
-const elementFactory = function (elementStr: string): void {
+const elementFactory = function (elementStr: string): HTMLElement {
     const commands = elementStr.split('/');
-    console.log(commands[0]);
+    const topLevelElement = document.createElement(commands[0]);
+    commands.shift();
+    commands.forEach((commandStr: string) => {
+        const attribute = commandStr.split('=')[0];
+        const values = commandStr.split('=')[1];
+        if (attribute.toLowerCase() === 'child') {
+            // Create child elements
+        }
+        topLevelElement.setAttribute(attribute, values);
+    });
+    return topLevelElement;
 };
 
-elementFactory('div/class=test');
+console.log(
+    elementFactory(
+        'div/class=test wide/id=someid/data-name=something'
+        )
+);
+
+// Include a content which is the textcontent
